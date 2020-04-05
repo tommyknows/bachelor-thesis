10ab0448d1f2aec8455f00308af82b3aee8abc5d
Ramon Rüttimann <me@ramonr.ch>
2020-04-05 21:27:20 +0200

    Auto-locate code and start refactoring thesis
    Signed-off-by: Ramon Rüttimann <me@ramonr.ch>

---
+++ thesis/utils/go.mod
@@ -0,0 +1,3 @@
+module github.zhaw.ch/ruettram/bachelor/thesis/utils
+
+go 1.14
---
+++ thesis/utils/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const (
+	targetFile       = "code"
+	snippetEnding    = ".snippet"
+	lineNumberEnding = ".lineno"
+)
+
+func main() {
+	start := flag.String("start", "", "the starting delimiter, uncommented")
+	end := flag.String("end", "", "the ending delimiter, uncommented")
+	sourceFile := flag.String("file", "", "the source file")
+
+	flag.Parse()
+
+	*start = "// " + *start
+	*end = "// " + *end
+
+	file, err := os.Open(*sourceFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	codeDest, err := os.Create(targetFile + snippetEnding)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer codeDest.Close()
+
+	lineDest, err := os.Create(targetFile + lineNumberEnding)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer lineDest.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	// find start
+	lineno := gotoStart(scanner, *start)
+	// write line number
+	_, _ = lineDest.WriteString(strconv.Itoa(lineno))
+	// write source cod
+	_, _ = codeDest.WriteString(strings.ReplaceAll(scanner.Text(), *start, "// ...") + "\n")
+
+	// scan until end and write
+	for scanner.Scan() {
+		if strings.TrimSpace(scanner.Text()) == *end {
+			_, _ = codeDest.WriteString(strings.ReplaceAll(scanner.Text(), *end, "// ...") + "\n")
+			break
+		}
+		_, _ = codeDest.WriteString(scanner.Text() + "\n")
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+	_ = codeDest.Sync()
+	_ = lineDest.Sync()
+}
+
+func gotoStart(s *bufio.Scanner, start string) (linenumber int) {
+	for s.Scan() {
+		if strings.TrimSpace(s.Text()) == start {
+			return linenumber
+		}
+		linenumber++
+	}
+	return -1
+}
