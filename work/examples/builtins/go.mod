7b664ffe001edf4c9be349d69ab4797d00cc855c
Ramon Rüttimann <ramon@nine.ch>
2020-05-24 20:16:00 +0200

    Small corrections and fixes everywhere
    Signed-off-by: Ramon Rüttimann <ramon@nine.ch>

--- work/examples/builtins/go.mod
+++ work/examples/builtins/go.mod
@@ -1,3 +1,8 @@
 module newbuiltins
 
 go 1.14
+
+require (
+	github.com/tommyknows/funcheck v0.1.0 // indirect
+	golang.org/x/tools/gopls v0.4.1 // indirect
+)
--- work/examples/builtins/main.go
+++ work/examples/builtins/main.go
@@ -5,18 +5,19 @@ import (
 	"strconv"
 )
 
-type parity bool
+// Counterpart to Haskell's `derive Show`
+//go:generate stringer -type parity
 
-const (
-	even parity = true
-	odd  parity = false
-)
+type parity int
+
+const even parity = 0
+const odd parity = 1
 
 // shouldBe returns a function that returns true if an int is of the
 // given parity
 func shouldBe(p parity) func(i int) bool {
 	return func(i int) bool {
-		return (i%2 == 0) == p
+		return i%2 == int(p)
 	}
 }
 
@@ -24,17 +25,14 @@ func main() {
 	l := []int{1, 2, 3, 4, 5}
 	l5 := fmap(func(i int) int { return i * 5 }, prepend(0, l))
 
+	addToString := func(s string, i int) string {
+		return s + strconv.Itoa(i) + " "
+	}
 	// fold over even / odd numbers and add them to a string
-	evens := foldl(
-		func(s string, i int) string { return s + strconv.Itoa(i) + " " },
-		"even: ",
-		filter(shouldBe(even), l5),
-	)
-	odds := foldl(
-		func(s string, i int) string { return s + strconv.Itoa(i) + " " },
-		"odd: ",
-		filter(shouldBe(odd), l5),
-	)
+	evens := foldl(addToString, even.String()+": ",
+		filter(shouldBe(even), l5))
+	odds := foldl(addToString, odd.String()+": ",
+		filter(shouldBe(odd), l5))
 
 	fmt.Println(evens, odds)
 }
