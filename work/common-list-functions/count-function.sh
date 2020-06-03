97ab251bc8757a16faa8b4bb1fb273d96ed0957e
Ramon Rüttimann <ramon@nine.ch>
2020-06-03 09:23:59 +0200

    further enhancements & a LICENSE file
    Signed-off-by: Ramon Rüttimann <ramon@nine.ch>

--- work/common-list-functions/count-function.sh
+++ work/common-list-functions/count-function.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Usage example:
+# ./count-function.sh ": " map fold filter take drop reverse sum zip product maximum minimum
+
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 echo "Searching for occurrences in subdirectories of $dir"
 for func in "$@"; do
-    occurrences="$(grep --include \*.hs -Ro " $func" "$dir" | wc -l | xargs)"
+    occurrences="$(rg --type hs " $func" --count-matches | awk -F ':' '{sum += $2} END {print sum}')"
     echo "Found $occurrences occurrences of $func"
 done
